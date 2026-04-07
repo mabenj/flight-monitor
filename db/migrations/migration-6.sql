1781a476d576652112c5d161b9981e8534599603
mabenj <markobenj@hotmail.com>
2026-04-07 20:58:14 +0300

    Revert "Drop foreign key in activeFlight table"
    This reverts commit 93e7953fe07929d8e3bd063a70a69b7cd542fae1.

--- db/migrations/migration-6.sql
+++
@@ -1,8 +0,0 @@
--- Drop the foreign key constraint on the activeFlight table. 
--- No need to migrate the data since the data is transient
-
-DROP TABLE activeFlight;
-
-CREATE TABLE activeFlight(
-    flightId TEXT PRIMARY KEY
-);
